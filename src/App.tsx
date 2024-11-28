a02a964d7399f9d4275b2a8bc811c3d7d9f82368
Tony Lim <tony.lim@sandfield.co.nz>
2024-11-29 12:24:42 +1300

    feat: Added navbar and MUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { useRef, useState } from 'react';
 import { IRefPhaserGame, PhaserGame } from './game/PhaserGame';
 import { MainMenu } from './game/scenes/MainMenu';
+import AppBarUsage from './components/AppBarUsage';
 
-function App()
-{
+function App() {
     // The sprite can only be moved in the MainMenu Scene
     const [canMoveSprite, setCanMoveSprite] = useState(true);
 
@@ -13,12 +13,10 @@ function App()
 
     const changeScene = () => {
 
-        if(phaserRef.current)
-        {     
+        if (phaserRef.current) {
             const scene = phaserRef.current.scene as MainMenu;
-            
-            if (scene)
-            {
+
+            if (scene) {
                 scene.changeScene();
             }
         }
@@ -26,13 +24,11 @@ function App()
 
     const moveSprite = () => {
 
-        if(phaserRef.current)
-        {
+        if (phaserRef.current) {
 
             const scene = phaserRef.current.scene as MainMenu;
 
-            if (scene && scene.scene.key === 'MainMenu')
-            {
+            if (scene && scene.scene.key === 'MainMenu') {
                 // Get the update logo position
                 scene.moveLogo(({ x, y }) => {
 
@@ -46,19 +42,17 @@ function App()
 
     const addSprite = () => {
 
-        if (phaserRef.current)
-        {
+        if (phaserRef.current) {
             const scene = phaserRef.current.scene;
 
-            if (scene)
-            {
+            if (scene) {
                 // Add more stars
                 const x = Phaser.Math.Between(64, scene.scale.width - 64);
                 const y = Phaser.Math.Between(64, scene.scale.height - 64);
-    
+
                 //  `add.sprite` is a Phaser GameObjectFactory method and it returns a Sprite Game Object instance
                 const star = scene.add.sprite(x, y, 'star');
-    
+
                 //  ... which you can then act upon. Here we create a Phaser Tween to fade the star sprite in and out.
                 //  You could, of course, do this from within the Phaser Scene code, but this is just an example
                 //  showing that Phaser objects and systems can be acted upon from outside of Phaser itself.
@@ -77,24 +71,27 @@ function App()
     const currentScene = (scene: Phaser.Scene) => {
 
         setCanMoveSprite(scene.scene.key !== 'MainMenu');
-        
+
     }
 
     return (
         <div id="app">
-            <PhaserGame ref={phaserRef} currentActiveScene={currentScene} />
-            <div>
+            <AppBarUsage />
+            <div id="game">
+                <PhaserGame ref={phaserRef} currentActiveScene={currentScene} />
                 <div>
-                    <button className="button" onClick={changeScene}>Change Scene</button>
-                </div>
-                <div>
-                    <button disabled={canMoveSprite} className="button" onClick={moveSprite}>Toggle Movement</button>
-                </div>
-                <div className="spritePosition">Sprite Position:
-                    <pre>{`{\n  x: ${spritePosition.x}\n  y: ${spritePosition.y}\n}`}</pre>
-                </div>
-                <div>
-                    <button className="button" onClick={addSprite}>Add New Sprite</button>
+                    <div>
+                        <button className="button" onClick={changeScene}>Change Scene</button>
+                    </div>
+                    <div>
+                        <button disabled={canMoveSprite} className="button" onClick={moveSprite}>Toggle Movement</button>
+                    </div>
+                    <div className="spritePosition">Sprite Position:
+                        <pre>{`{\n  x: ${spritePosition.x}\n  y: ${spritePosition.y}\n}`}</pre>
+                    </div>
+                    <div>
+                        <button className="button" onClick={addSprite}>Add New Sprite</button>
+                    </div>
                 </div>
             </div>
         </div>
---
+++ src/components/AppBarUsage.tsx
@@ -0,0 +1,99 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import Avatar from '@mui/material/Avatar';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+
+const settings = ['Neighbours', 'Help', 'Settings'];
+
+export default function AppBarUsage() {
+    const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
+
+    const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorElUser(event.currentTarget);
+    };
+
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
+
+    return (
+        <AppBar position="static" sx={{ backgroundColor: "white", color: "black" }}>
+            <Toolbar sx={{ mx: 3 }}>
+                <Typography
+                    variant="h5"
+                    noWrap
+                    component="a"
+                    sx={{
+                        mr: 2,
+                        display: { xs: 'none', md: 'flex' },
+                        fontWeight: 700,
+                        color: 'inherit',
+                        textDecoration: 'none',
+                    }}
+                >
+                    John Doe
+                </Typography>
+                <Box sx={{ flexGrow: 0, ml: 3, }}>
+                    Level 23
+                </Box>
+                <Typography
+                    variant="h5"
+                    noWrap
+                    component="a"
+                    href="#app-bar-with-responsive-menu"
+                    sx={{
+                        mr: 2,
+                        display: { xs: 'flex', md: 'none' },
+                        flexGrow: 1,
+                        fontWeight: 700,
+                        color: 'inherit',
+                        textDecoration: 'none',
+                    }}
+                >
+                    John Doe
+                </Typography>
+                <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}></Box>
+                <Box sx={{ flexGrow: 0, mr: 5, }}>
+                    {/* TODO: REPLACE WITH COIN ICON */}
+                    Coins: 52
+                </Box>
+                <Box sx={{ flexGrow: 0 }}>
+
+                    <Tooltip title="Open menu">
+                        <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                            <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                        </IconButton>
+                    </Tooltip>
+                    <Menu
+                        sx={{ mt: '45px' }}
+                        id="menu-appbar"
+                        anchorEl={anchorElUser}
+                        anchorOrigin={{
+                            vertical: 'top',
+                            horizontal: 'right',
+                        }}
+                        keepMounted
+                        transformOrigin={{
+                            vertical: 'top',
+                            horizontal: 'right',
+                        }}
+                        open={Boolean(anchorElUser)}
+                        onClose={handleCloseUserMenu}
+                    >
+                        {settings.map((setting) => (
+                            <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                                <Typography sx={{ textAlign: 'center' }}>{setting}</Typography>
+                            </MenuItem>
+                        ))}
+                    </Menu>
+                </Box>
+            </Toolbar>
+        </AppBar>
+    );
+}
