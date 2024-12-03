5a74729fac8fdd5d599c4bc0b895d8063c7e3533
Tony Lim <tony.lim@sandfield.co.nz>
2024-12-03 14:15:09 +1300

    feat: Changed html structure and page spacing

--- src/components/content/NeighboursContent.tsx
+++ src/components/content/NeighboursContent.tsx
@@ -9,8 +9,8 @@ export default function NeighboursContent() {
         <div>
             <h1>Neighbours</h1>
             <div className="content">
-                <div>
-                    <p className="content-left">
+                <div className="content-left">
+                    <p>
                         Add people as neighbors and choose where they are on
                         your map, the higher up on this list the closer they
                         will be to your plot. Random players will appear around
--- src/components/content/SettingsContent.tsx
+++ src/components/content/SettingsContent.tsx
@@ -2,6 +2,13 @@ export default function SettingsContent() {
     return (
         <div>
             <h1>Settings</h1>
+            <div className="content">
+                <div>
+                    <div className="content-left">
+                        <h3>Emails</h3>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
