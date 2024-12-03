17dbd2195583952de581ee500774a8b01ccff79e
Tony Lim <tony.lim@sandfield.co.nz>
2024-12-03 14:05:12 +1300

    feat: Added info for help page

--- src/components/content/HelpContent.tsx
+++ src/components/content/HelpContent.tsx
@@ -2,6 +2,17 @@ export default function HelpContent() {
     return (
         <div>
             <h1>Help</h1>
+            <div className="content">
+                <div>
+                    <h3>Goal</h3>
+                    <p className="content-left">
+                        SandyGardens is a low-time mindfulness game to turn
+                        timesheets into an activity people look forward to
+                        completing. Build your garden any way you like and see
+                        what others are building as well.
+                    </p>
+                </div>
+            </div>
         </div>
     );
 }
