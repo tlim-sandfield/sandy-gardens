69e5104cd711e54d90573795d1423cf8583efc21
Tony Lim <tony.lim@sandfield.co.nz>
2024-12-11 10:29:35 +1300

    fix: Fixed spacing for settings page

--- src/components/content/SettingsContent.tsx
+++ src/components/content/SettingsContent.tsx
@@ -5,34 +5,32 @@ export default function SettingsContent() {
         <div>
             <h1>Settings</h1>
             <div className="content">
-                <div>
-                    <div className="content-left">
-                        <h3>Emails</h3>
-                        <FormGroup>
-                            <FormControlLabel
-                                control={
-                                    <Checkbox
-                                        defaultChecked
-                                        sx={{
+                <div className="content-left">
+                    <h3>Emails</h3>
+                    <FormGroup>
+                        <FormControlLabel
+                            control={
+                                <Checkbox
+                                    defaultChecked
+                                    sx={{
+                                        color: "primary",
+                                        "&.Mui-checked": {
                                             color: "primary",
-                                            "&.Mui-checked": {
-                                                color: "primary",
-                                            },
-                                        }}
-                                    />
-                                }
-                                label="Send me emails related to Sandy Gardens"
-                            />
-                            <FormControlLabel
-                                control={<Checkbox />}
-                                label="Required"
-                            />
-                            <FormControlLabel
-                                control={<Checkbox />}
-                                label="Required"
-                            />
-                        </FormGroup>
-                    </div>
+                                        },
+                                    }}
+                                />
+                            }
+                            label="Send me emails related to Sandy Gardens"
+                        />
+                        <FormControlLabel
+                            control={<Checkbox />}
+                            label="Required"
+                        />
+                        <FormControlLabel
+                            control={<Checkbox />}
+                            label="Required"
+                        />
+                    </FormGroup>
                 </div>
             </div>
         </div>
