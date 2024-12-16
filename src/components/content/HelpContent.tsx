bebbf979b6b2051726b940bb9b9d0ea4813ba3dd
Tony Lim <tony.lim@sandfield.co.nz>
2024-12-17 10:17:34 +1300

    feat: Added movement control instructions to help page

--- src/components/content/HelpContent.tsx
+++ src/components/content/HelpContent.tsx
@@ -11,6 +11,15 @@ export default function HelpContent() {
                         completing. Build your garden any way you like and see
                         what others are building as well.
                     </p>
+                    <h3>Movement Controls</h3>
+                    <p>
+                        <strong>Scroll wheel:</strong> scroll up and down <br />
+                        <strong>CTRL + scroll wheel:</strong> zoom in and out <br />
+                        <strong>SPACE + left click and drag:</strong> pan the
+                        camera <br />
+                        <strong>Scroll button click and drag:</strong> pan the
+                        camera
+                    </p>
                 </div>
             </div>
         </div>
