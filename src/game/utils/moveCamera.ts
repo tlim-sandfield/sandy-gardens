28e2c5a0f98a145ad0d98d423ab43d514227bbf3
Tony Lim <tony.lim@sandfield.co.nz>
2024-12-16 13:59:10 +1300

    fix: prevent zooming in or out too much

--- src/game/utils/moveCamera.ts
+++ src/game/utils/moveCamera.ts
@@ -19,17 +19,23 @@ export default function moveCamera({
     if (keyCtrl?.isDown) {
         // ZOOM: mouse wheel up and down
         const zoomChange = pointer.deltaY * -ZOOM_SPEED;
-
-        const pointerX = pointer.x;
-        const pointerY = pointer.y;
-        const cameraX = camera.scrollX + camera.width / 2;
-        const cameraY = camera.scrollY + camera.height / 2;
-        const dx = pointerX - cameraX;
-        const dy = pointerY - cameraY;
-
         camera.zoom += zoomChange;
-        camera.scrollX += dx * zoomChange;
-        camera.scrollY += dy * zoomChange;
+
+        if (camera.zoom < 0.25) {
+            camera.zoom = 0.25;
+        } else if (camera.zoom > 2) {
+            camera.zoom = 2;
+        } else {
+            const pointerX = pointer.x;
+            const pointerY = pointer.y;
+            const cameraX = camera.scrollX + camera.width / 2;
+            const cameraY = camera.scrollY + camera.height / 2;
+            const dx = pointerX - cameraX;
+            const dy = pointerY - cameraY;
+
+            camera.scrollX += dx * zoomChange;
+            camera.scrollY += dy * zoomChange;
+        }
     } else {
         // SCROLL: mouse wheel up and down
         const cameraYChange =
