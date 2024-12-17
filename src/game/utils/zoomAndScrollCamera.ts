656f098ecb8f71d52f1eb70791b68db2fc2fd8e4
Tony Lim <tony.lim@sandfield.co.nz>
2024-12-18 11:20:43 +1300

    fix: Simplified zooming motion

--- src/game/utils/zoomAndScrollCamera.ts
+++ src/game/utils/zoomAndScrollCamera.ts
@@ -18,23 +18,12 @@ export default function zoomAndScrollCamera({
 
     if (keyCtrl?.isDown) {
         // ZOOM: mouse wheel up and down
-        const zoomChange = pointer.deltaY * -ZOOM_SPEED;
-        camera.zoom += zoomChange;
+        camera.zoom += pointer.deltaY * -ZOOM_SPEED;
 
         if (camera.zoom < 0.15) {
             camera.zoom = 0.15;
         } else if (camera.zoom > 1.5) {
             camera.zoom = 1.5;
-        } else {
-            const pointerX = pointer.x;
-            const pointerY = pointer.y;
-            const cameraX = camera.scrollX + camera.width / 2;
-            const cameraY = camera.scrollY + camera.height / 2;
-            const dx = pointerX - cameraX;
-            const dy = pointerY - cameraY;
-
-            camera.scrollX += dx * zoomChange;
-            camera.scrollY += dy * zoomChange;
         }
     } else {
         // SCROLL: mouse wheel up and down
